3adf773e2db4cf0a431e5f8bc173a16ba0126079
Anders <anders.lundberg@live.no>
2024-09-10 19:22:31 +0200

    Included hangarounds and pangs. Prints excluded ches

--- main.py
+++ main.py
@@ -1,24 +1,23 @@
 import pandas as pd
 import numpy as np
-from collections import defaultdict
+from collections import defaultdict, Counter
 import random
 from datetime import datetime
 
 #Path til vaktliste
-file_path = 'vaktliste2Uker.xlsx'
+file_path = "Vaktønsker Lyche Kjøkken V24 uke 39 & 40.xlsx"
 xls = pd.ExcelFile(file_path)
 
 # Load the sheet into a dataframe
 sheet_name = 0
 df = pd.read_excel(file_path, sheet_name=sheet_name)
-print(df.head(10))
-
-#Extract chef names
+#print(df.head(10))
 
 
 # Extract the dates into a list
+date_format = '%Y-%m-%d %H:%M:%S' # Date format here must be equal to format in dataframe above
 dates = df.iloc[0, 1:].dropna().tolist()
-formatted_dates = [datetime.strptime(str(date), '%Y-%m-%d %H:%M:%S').strftime('%d/%m ') for date in dates]
+formatted_dates = [datetime.strptime(str(date), date_format).strftime('%d/%m ') for date in dates]
 double_dates = [date for date in formatted_dates for _ in range(2)]
 #print('Dates: ', double_dates)
 
@@ -26,6 +25,8 @@ double_dates = [date for date in formatted_dates for _ in range(2)]
 time_slots = df.iloc[1, 1::1].dropna().tolist() #Len = 16
 compressed_time_slots = [time_slot[:2] + '-' + time_slot[8:10] for time_slot in time_slots]
 #print('Time slots: ', compressed_time_slots)
+
+
 
 #Create shift names
 shift_names = []
@@ -37,18 +38,29 @@ for n in range(len(double_dates)):
 # Extract relevant data from df and insert to another dataframe
 start_of_chef_list = 3 
 chefs_availability = df.iloc[start_of_chef_list:, :29]
+chefs_availability.columns = ['Kokk'] + shift_names
 #print(chefs_availability.head(10))
+
+
+hangarounds_row_index = chefs_availability[chefs_availability['Kokk'].str.contains("HANGAROUNDS", case=False, na=False)].index
+
+
+# Remove "AKTIVE" "HANGAROUNDS", "PANGER" AND NaN
+chefs_availability = chefs_availability[
+    ~chefs_availability['Kokk'].isin(["AKTIVE", "HANGAROUNDS", "PANGER"]) & 
+    chefs_availability['Kokk'].notna()
+]
 
 #If Chef have not doodled => available all shifts, except from hangarounds or pangs
 for index, row in chefs_availability.iterrows():
+   if index >= hangarounds_row_index[0]:
+       break
    if row[1:].isna().all():
        chefs_availability.loc[index, row.index[1:]] = "Kan jobbe!"
 
-#print(chefs_availability.head(10))
+#print(chefs_availability[45:60])
 
-# Set column names of dataframe
-chefs_availability.columns = ['Chef'] + shift_names
-#print(chefs_availability.head(10))
+
 
 week_1_df = chefs_availability.iloc[:, 0:15]
 week_2_df = chefs_availability.iloc[:, [0] + list(range(15, 29))]
@@ -56,27 +68,40 @@ week_2_df = chefs_availability.iloc[:, [0] + list(range(15, 29))]
 #print(week_2_df.head(10))
 
 
-
-
 # Returns a lsit for available chefs on shift 'dd/mm hh-hh'
 def get_available_chefs(shift, temp_df):
-    return temp_df[temp_df[shift] == "Kan jobbe!"]['Chef'].tolist()
+    return temp_df[temp_df[shift] == "Kan jobbe!"]['Kokk'].tolist()
 
 #returns number of chefs needed for the shift
 def get_num_of_chefs(shift_name):
     if '13-21' in shift_name:
         num_chefs = 3
     else:
-        num_chefs =4
+        num_chefs = 4
     return num_chefs
 
+""" def have_enough_old_chefs(list_of_chefs, availability_df):
+    num_old_chefs = 0
+    chef_column = availability_df.iloc[:, 0]
+    for c in list_of_chefs:
+        chef_row_index = chef_column[chef_column.str.contains(c, case=False, na=False)].index
+        print(chef_row_index[0])
+        val = availability_df.iloc[chef_row_index[0],1]
+        if val == 1:
+            num_old_chefs += 1
+    
+    enough_old_chefs = num_old_chefs >= 1
+    
+    
+    return enough_old_chefs """
 
-def assign_chefs(availability_df): # Input parameters: week_x_df, (shift_names, lag funksjon til denne)
+
+def assign_chefs(availability_df):
 
     schedule = defaultdict(list) #Dictionary for the schedule
 
     #Sort the shifst by availability low to high
-    df_without_chefs = availability_df.drop(columns=['Chef'])
+    df_without_chefs = availability_df.drop(columns=['Kokk'])
     kan_jobbe_count = df_without_chefs.apply(lambda col: col.value_counts().get("Kan jobbe!", 0))
     kan_jobbe_count_dict = kan_jobbe_count.to_dict()
 
@@ -94,15 +119,17 @@ def assign_chefs(availability_df): # Input parameters: week_x_df, (shift_names, 
     #while not schedule_finished:
     temp_df = availability_df.copy()
     for shift in sorted_shifts:
-        chefs = get_available_chefs(shift, temp_df)
-        random.shuffle(chefs)
         number_of_chefs = get_num_of_chefs(shift)
-        selected_chefs = chefs[:number_of_chefs]
+        available_chefs = get_available_chefs(shift, temp_df)
+        random.shuffle(available_chefs)
+        selected_chefs = available_chefs[:number_of_chefs]
         schedule[shift] = selected_chefs
         for chef in selected_chefs:
             for s in sorted_shifts:
                 if s != shift:
-                    temp_df.loc[temp_df['Chef'] == chef, s] = "Opptatt"
+                    temp_df.loc[temp_df['Kokk'] == chef, s] = "Opptatt"
+
+    
 
     schedule_df = pd.DataFrame.from_dict(schedule, orient='index') # Load dictionary to dataframe
     schedule_df.columns = [f"Kokk {i+1}" for i in range(schedule_df.shape[1])] # Set column names
@@ -118,28 +145,62 @@ def assign_chefs(availability_df): # Input parameters: week_x_df, (shift_names, 
     schedule_sorted_df.index = schedule_sorted_df.index.strftime('%d/%m %H-%M')
 
     # Print the sorted DataFrame
-    #print(df_sorted)
+    #print(schedule_sorted_df)
 
     return schedule_sorted_df
 
+def check_schedule(schedule_df):
+    registered_chefs = df.iloc[3:hangarounds_row_index[0], 0].dropna().tolist()
+    assigned_chefs = schedule_df.values.ravel().tolist()
+    assigned_chefs = [name for name in assigned_chefs if name != None]
+    #print(f'Registered chefs: {len(registered_chefs)}')
+    #print(f'Assigned chefs: {len(assigned_chefs)}')
+    excluded_chefs = []
+
+    # Check for duplicates
+    counts = Counter(assigned_chefs)
+    duplicate_chefs = [item for item, count in counts.items() if count > 1]
+    if len(duplicate_chefs) == 0:
+        duplicate_chefs.append('None')
+
+    # Check for excluded chefs
+    for chef in registered_chefs:
+        if chef not in assigned_chefs:
+            excluded_chefs.append(chef)
+
+    if len(excluded_chefs) < 3:
+        schedule_finished = True
+    else:
+        schedule_finished = False
+
+    # Write to excel file
+    print(f'Excluded chefs: {excluded_chefs}')
+    #print(f'Duplicated chefs: {duplicate_chefs}')
+
+    return schedule_finished
+      
+
 shift_schedule_week_1 = assign_chefs(week_1_df)
+check_schedule(shift_schedule_week_1)
+
 shift_schedule_week_2 = assign_chefs(week_2_df)
+check_schedule(shift_schedule_week_2)
+
 
 file_path_week_1 = 'Chef_Shifts_Week_1.xlsx'
 file_path_week_2 = 'Chef_Shifts_Week_2.xlsx'
 shift_schedule_week_1.to_excel(file_path_week_1, index_label = 'Skift uke 1')
 shift_schedule_week_2.to_excel(file_path_week_2, index_label = 'Skift uke 2')
 
+
 print(f"Schedule saved to {file_path_week_1} and {file_path_week_2}")
 
 
 #TODO
-# Last opp til GitHub
 # Sjekke at alle kokker er inkludert og lage metode for å inkludre kokker som er til overs
-# Inkludere Hangarounds og Panger som har doodlet
-# Fjerne rader der navn = ''. aktive, hangarounds, panger
 # Legge inn håndtering av 'Kan om nødvendig'
-# Legge inn eventuelle error-meldinger
+# Skille mellom nye og gamle kokker
+# Håndtering av vanlige errors
 # Fikse slik at det fungerer med ulike antall sheets og brøkdeler av uker
 
-# Trenger 51 kokker. Vi er 37. Hvor mange skal vi bli? N 
+
